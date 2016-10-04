e25eb4835cab7406014b1dd4918f7453756c1897
Pete <petergarvin1@gmail.com>
2016-10-04 19:53:18 +0100

    add scripts

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+<script src="https://cdn.rawgit.com/michalsnik/aos/2.0.4/dist/aos.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
 AOS.init({
   duration: 1200
 });
